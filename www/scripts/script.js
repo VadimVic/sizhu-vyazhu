b39612c15a26e8cfd44396b78af029523913563f
Vadim Bykov <nordvadim@bk.ru>
2021-08-17 22:18:46 +0300

    сделана большая часть работы

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,32 @@
-console.log('script work!');
+  $(document).ready(function(){
+    // $('.js-slider').slick({
+    //   dots: true,
+    //   autoplay: true,
+    //   infinite: true,
+    //   slidesToShow: 1,
+    //   slidesToScroll: 1
+    // });
+
+  // // // // Делаем карусель в секции photos
+  $('.js-click-slider').each(function (index, sliderWrap) {
+    let slider = $(sliderWrap).find('.js-slider');
+    let btnPrev = $(sliderWrap).find('.js-btn-prev');
+    let btnNext = $(sliderWrap).find('.js-btn-next');
+
+    $(slider).slick({
+      // autoplay: true,
+      dots: true ,
+      prevArrow: btnPrev,
+      nextArrow: btnNext,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
+
+  $('.burger').on('click', function () {
+    $('.main-nav').slideToggle();
+  });
+
+  });
+
+
